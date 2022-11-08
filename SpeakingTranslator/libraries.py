d1711ba6da5ecad4e7ce18982ad0ad516e51d0e4
laura martel <lpm.martel@gmail.com>
2022-11-08 12:04:36 +0100

    Libraries added

---
+++ SpeakingTranslator/libraries.py
@@ -0,0 +1,13 @@
+from tkinter import *
+import tkinter
+import textblob
+from tkinter import messagebox
+from tkinter import ttk
+import random
+from random import choice 
+from PIL import Image, ImageDraw
+import io
+
+
+root = Tk()
+root.title("Bad Translator")
